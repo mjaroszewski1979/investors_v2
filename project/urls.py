8c0ce66f7924823a911ddb7d59b4c6bee381e874
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-03-27 20:18:17 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('investors.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
