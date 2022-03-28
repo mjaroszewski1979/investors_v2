22dacb7f1556e0de6ae042b0adc0a6720a1d14e9
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-03-28 21:10:37 +0200

    Update admin.py

--- investors/admin.py
+++ investors/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from . import models
 
+class InvestorsAdminArea(admin.AdminSite):
+    site_header = 'Investors Admin Area'
+
+investors_site = InvestorsAdminArea(name='InvestorsAdmin')
+
 admin.site.register(models.Country)
 admin.site.register(models.Fund)
 admin.site.register(models.Investor)
+
+investors_site.register(models.Country)
+investors_site.register(models.Fund)
+investors_site.register(models.Investor)
