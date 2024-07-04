e40b7b259eaf0af7701bcbf91dc9757458574281
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-04 19:46:14 +0200

    Update tests.py

--- investors/tests.py
+++ investors/tests.py
@@ -1,12 +1,22 @@
+# Django imports
 from django.urls import reverse, resolve
 from django.test import TestCase, Client
+
+# App imports
 from . import views, models, utilities
 
 
 
 class CatalogTest(TestCase):
+    """
+    Test case for the catalog app functionalities, including URL resolutions, view responses, and utility functions.
+    """
 
     def setUp(self):
+        """
+        Set up the initial conditions for the tests.
+        This method initializes a test client and creates instances of Country, Fund, and Investor models.
+        """
         self.client = Client()
         self.country_1 = models.Country(name='united states', population=300000000)
         self.country_1.save()
@@ -26,15 +36,26 @@ class CatalogTest(TestCase):
 
 
     def test_index_url_is_resolved(self):
+        """
+        Test if the URL for the index view is resolved correctly.
+        """
         url = reverse('index')
         self.assertEquals(resolve(url).func.view_class, views.HomeView)
 
     def test_index_get(self):
+        """
+        Test the GET request to the index view.
+        This method checks the response status, template used, and content.
+        """
         response = self.client.get(reverse('index'))
         self.assertContains(response, 'Famous Investors', status_code=200)
         self.assertTemplateUsed(response, 'index.html')
 
     def test_uitilities(self):
+        """
+        Test the utility functions for calculating averages.
+        This method checks the population average, age average, AUM average, and fund type.
+        """
         data = utilities.QueryAttributes()
         self.assertEquals(data.population_avg, 175000000)
         self.assertEquals(data.age_avg, 37)
