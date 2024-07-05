22d3964eb41602b006b32c4e0934700143aff6f9
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-05 11:17:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-
-
+# Apply any pending database schema changes based on model changes
 python manage.py makemigrations 
+
+# Apply those changes to the database
 python manage.py migrate 
+
+# Create a superuser for the Django admin interface
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell 
+
+# Start the Django development server on all available network interfaces, port 8000
 python manage.py runserver 0.0.0.0:8000
 
 
